84cf67963b9acd94585cb32b1bfc15e6824d74c4
Keith <dreamihad@gmail.com>
2021-12-31 20:07:12 -0500

    cmd done

--- SetUp.py
+++ SetUp.py
@@ -1,5 +1,6 @@
 from Utils import Utils
 
+import getpass
 
 class SetUp:
 
@@ -10,8 +11,8 @@ class SetUp:
         print("What would you like to do")
         print("Setup SSH and Wifi access on a PI SD CARD, 1")
         print("Open a GUI since this is hard, 2")
-        print("Get IP addresses of PIs on your network, 3")
-        Number = int(input("Make a selection"))
+        print("Get IP addresses of PIs on your network, 3 \n")
+        Number = int(input("Make a selection \n"))
         if Number == 1:
             self.setup_wifi()
         if Number == 2:
@@ -19,7 +20,27 @@ class SetUp:
         if Number == 3: 
             self.list_pi_ips()
     def setup_wifi(self):
-        self.restart()
+        #Number = int(input("Make a selection \n"))
+        target_drive =     input("Target Drive \n") 
+        config_path , right_dir = self.utils.set_drive(target_drive) 
+        
+        if  right_dir:          
+          
+
+            ssid = input("SSID \n")
+            password = self.get_password()
+            self.utils.write_files(ssid,password)
+        self.restart() 
+
+    def get_password(self):
+        password = getpass.getpass("Password \n")
+        password_confirm = getpass.getpass("Confirm Password \n")
+
+        
+        if password != password_confirm:
+            print("Passwords do not match")
+            return self.get_password()
+        return password   
     def open_gui(self):
         self.restart()
     def list_pi_ips(self):
@@ -29,7 +50,7 @@ class SetUp:
         
     def restart(self):
         print('Restart ?')
-        Number = int(input("1 to restart, 2 to exit"))
+        Number = int(input("1 to restart, 2 to exit\n"))
         if Number == 1 :
             self.main_menu()
 
--- Utils.py
+++ Utils.py
@@ -21,25 +21,40 @@ class Utils:
         file_to_cp = self.get_local_file_path(almost_file) 
         new_file_path = os.path.join(self.found_drive,local_file_location) 
         shutil.copy( file_to_cp, new_file_path)
+        print("Created ", new_file_path)
         return new_file_path 
 
-    def get_local_file_path(sub_path):
+    def get_local_file_path(self,sub_path):
         dir_path = os.path.dirname(os.path.realpath(__file__))
         return os.path.join(dir_path,sub_path)
-    def select_drive(self):    
-        self.found_drive = tk.filedialog.askdirectory()
+    def set_drive(self,found_drive):    
+        self.found_drive =  found_drive    
         print('Using ' +  self.found_drive)   
+        expected_config_path = os.path.join(self.found_drive,'config.txt')
+        drive_exist = os.path.exists(expected_config_path)
+        # check for config files, to make sure we're in the right place 
+        if drive_exist:
+            return True , expected_config_path
 
+        
+        print(expected_config_path , ' : not found , check your path' )
+           # return False , expected_config_path
+        return False , expected_config_path
     def write_files(self,ssid,password):
+
+      
         self.copy_file('ssh')
         wpa_file_path = self.copy_file('wpa_supplicant.conf')
         self.replace_text_in_file(wpa_file_path,'your_network_name',ssid)
         self.replace_text_in_file(wpa_file_path,'your_wifi_password',password)
+        return "PI SDCard Ready for SSH Over wifi"
+        
 
     def find_pi(self):
         all_lines = subprocess.check_output('arp -a', universal_newlines=True).split('\n')
         arr =[] 
         for device in all_lines:
+            print(device)
             for possibleMac in self.pi_mac_address_ranges:
                 if possibleMac.lower() in device.lower():
                     arr.append(device)
